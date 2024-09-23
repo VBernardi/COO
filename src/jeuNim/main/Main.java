c7312efad883bf2fded85e6c6259b7da99d98132
VBernardi <vincent.bernardi@etu.univ-orleans.fr>
2024-09-23 17:46:22 +0200

    modif

--- src/jeuNim/main/Main.java
+++ src/jeuNim/main/Main.java
@@ -17,7 +17,7 @@ public class Main {
         boolean continuer = true;
         while (continuer) {
             System.out.println("Choisissez votre façon de jouer");
-            System.out.println("0 : classique, 1 : Fan Tan, 2 : 1 + choix nb max allumettes retirable par coup" );
+            System.out.println("0 : classique, 1 : Fan Tan, 2 : Fan Tan + choix nb max allumettes retirable par coup" );
 
         
             String jeu = sc.nextLine();
@@ -29,8 +29,11 @@ public class Main {
                     controleurJeuNim = new ControleurJeuNim(ihm);
                 } else if (jeu.equals("1")){
                     controleurJeuNim = new ControleurFanTan(ihm);
-                } else{
+                } else if (jeu.equals("2")){
                     controleurJeuNim = new ControleurNimMax(ihm);
+                } else {
+                    System.out.println("Jeu avec les paramètres de base");
+                    controleurJeuNim = new ControleurJeuNim(ihm);
                 }
                 controleurJeuNim.lancerPartie();
             }
